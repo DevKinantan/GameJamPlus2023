94ca50cf0c6a73cc4b5c70c14b6c816af38ca547
Ichlasul Bulqiah <bichlasulbulqiah@gmail.com>
2023-10-27 11:14:10 +0700

    add init project stuff

---
+++ scripts/Enemy.gd
@@ -0,0 +1,28 @@
+extends StaticBody2D
+
+var death_body_scene = preload("res://scenes/death_enemy.tscn")
+var hp = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_hurtbox_area_entered(area):
+	print(area.name)
+	#spawn_death_body()
+	hp -= 1
+	if hp == 0:
+		call_deferred("spawn_death_body")
+		queue_free()
+
+
+func spawn_death_body():
+	var death_body_node = death_body_scene.instantiate()
+	get_parent().add_child(death_body_node)
+	death_body_node.global_position = global_position
---
+++ scripts/Player.gd
@@ -0,0 +1,88 @@
+extends CharacterBody2D
+
+
+enum {
+	MOVE,
+	ATTACK,
+}
+
+const SPEED = 100.0
+
+var is_facing_left = false
+var state = MOVE
+
+@onready var animationPlayer = $AnimationPlayer
+@onready var idleSprite = $IdleSprite
+@onready var walkSprite = $WalkSprite
+@onready var attackSprite = $AttackSprite
+@onready var hitboxPivot = $HitboxPivot
+@onready var hitbox = $HitboxPivot/Hitbox
+
+
+func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ATTACK:
+			play_attack()
+
+
+func move_state(delta):
+	var direction = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	direction = direction.normalized()
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+	elif direction.x or direction.y:
+		play_walk()
+		direction *= SPEED
+		velocity = Vector2(direction.x, direction.y)
+		if direction.x:
+			if (direction.x < 0 and not is_facing_left) or (direction.x > 0 and is_facing_left):
+				flip_sprite()
+	else:
+		play_idle()
+		velocity *= delta
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	move_and_slide()
+
+
+func play_walk():
+	walkSprite.visible = true
+	idleSprite.visible = false
+	attackSprite.visible = false
+	animationPlayer.play("WalkRight")
+
+
+func play_idle():
+	walkSprite.visible = false
+	idleSprite.visible = true
+	attackSprite.visible = false
+	animationPlayer.play("Idle")
+
+
+func play_attack():
+	walkSprite.visible = false
+	idleSprite.visible = false
+	attackSprite.visible = true
+	hitbox.monitorable = true
+	animationPlayer.play("AttackRight")
+
+
+func flip_sprite():
+	is_facing_left = not is_facing_left
+	idleSprite.flip_h = is_facing_left
+	walkSprite.flip_h = is_facing_left
+	attackSprite.flip_h = is_facing_left
+	
+	if is_facing_left:
+		hitboxPivot.rotation_degrees = 180
+	else:
+		hitboxPivot.rotation_degrees = 0
+
+
+func attack_animation_finish():
+	state = MOVE
+	hitbox.monitorable = false
